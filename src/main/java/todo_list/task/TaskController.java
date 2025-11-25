ac69c0816b8561afc70a5fea2da1ddcd3bbe21e3
isabella <isabellarottoli.tech@gmail.com>
2025-11-25 10:32:31 -0300

    feat: endpoint to create task

--- src/main/java/todo_list/task/TaskController.java
+++ src/main/java/todo_list/task/TaskController.java
@@ -3,9 +3,9 @@ package todo_list.task;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -19,5 +19,11 @@ public class TaskController {
     @GetMapping("/")
     public List<TaskModel> list(HttpServletRequest request){
         return taskRepository.findAll();
-    } 
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Void> create(@RequestBody TaskModel taskModel){
+        taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
